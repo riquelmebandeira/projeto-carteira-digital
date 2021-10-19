8dd701b2641e4d4a28985e54e9c24da625529e15
Riquelme <riquelmebandeira1@outlook.com>
2021-10-18 21:44:27 -0300

    Requisito 4: concluido

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/carteira" component={ Wallet } />
       <Route component={ NotFound } />
     </Switch>
   );
